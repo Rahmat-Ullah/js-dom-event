82f59de34d66ecbfa2475c1db53decbb3339eff4
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-25 07:02:17 +0600

    more onclick and addeventlistener funtction practiced

--- js/event.js
+++ js/event.js
@@ -1,3 +1,5 @@
+//option 1 directly on html tag
+//<button title="this is a tooltip" onclick="console.log(7)">click me</button>
 
 // option 2
 function makered(){
@@ -19,3 +21,19 @@ const makepurple = document.getElementById('make-purple');
         makepurple.onclick = function makePurple(){
             document.body.style.backgroundColor = 'purple';
         }
+
+// option 4
+const makepink = document.getElementById('make-pink');
+        makepink.addEventListener('click', makePink);
+
+        function makePink(){
+            document.body.style.backgroundColor = 'pink';
+        }
+
+// option 4 another
+ const makegreen = document.getElementById('make-green');
+        makegreen.addEventListener('click', function makegreen(){
+            document.body.style.backgroundColor = 'green';
+        });
+
+// option 4 final
