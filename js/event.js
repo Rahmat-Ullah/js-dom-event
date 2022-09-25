da9290b3b94ae4adcf1e6858f55640c7bf5bd0fa
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-25 06:41:11 +0600

    onclick 4 option practiced

--- js/event.js
+++ js/event.js
@@ -1,1 +1,21 @@
-console.log('this is external js file.');
+
+// option 2
+function makered(){
+    document.body.style.backgroundColor = 'red';
+}
+
+//option 3
+
+const makebluebutton = document.getElementById('make-blue');
+        makebluebutton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        }
+
+//option 3 another
+
+const makepurple = document.getElementById('make-purple');
+        makepurple.onclick = function makePurple(){
+            document.body.style.backgroundColor = 'purple';
+        }
